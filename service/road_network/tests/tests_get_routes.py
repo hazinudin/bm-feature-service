8e9aef932fd0a7052a76eb41296100446b02b26c
hazinudin <hazinudin@hotmail.com>
2025-03-14 13:35:45 +0700

    Update tests

--- service/road_network/tests/tests_get_routes.py
+++ service/road_network/tests/tests_get_routes.py
@@ -10,10 +10,10 @@ import grpc
 def run():
     with grpc.insecure_channel("localhost:50052") as channel:
         stub = lrs_pb2_grpc.RoadNetworkStub(channel)
-        routes_req = lrs_pb2.DownloadRequests(routes=['01001', '01002', '01003'], output_shp='SegmenRuas_Prov_01.shp')
+        route_req = lrs_pb2.RouteRequests(routes=['01001'])
+        routes = stub.GetByRouteId(route_req)
 
-        file_path = stub.DownloadAsSHP(routes_req)
-        print(f"Request: {routes_req}, Response: {file_path}")
+        print(routes)
 
 if __name__ == '__main__':
     logging.basicConfig()
